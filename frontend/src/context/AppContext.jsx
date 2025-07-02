525d663ffe043684d4d1e5e83da9abfceb4e4015
ANKUL KUMAR <152960667+ankulkumar2005@users.noreply.github.com>
2025-07-03 00:34:17 +0530

    Update AppContext.jsx

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext()
 const AppContextProvider = (props) => {
 
     const currencySymbol = '₹'
-    const backendUrl = https://medixcare-backend.onrender.com
+    const backendUrl = "https://medixcare-backend.onrender.com"
 
     const [doctors, setDoctors] = useState([])
     const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '')
